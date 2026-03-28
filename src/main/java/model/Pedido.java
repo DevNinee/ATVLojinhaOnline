cf998f20e75c9d4c418b1306e58c548efc317e13
Camile Eduarda <165083973+CamileECF@users.noreply.github.com>
2026-03-27 22:05:04 -0300

    adição do model pedido

--- src/main/java/model/Pedido.java
+++ src/main/java/model/Pedido.java
@@ -1,5 +1,53 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class Pedido {
 
+    private int idPedido;
+    private LocalDateTime data;
+    private String status;
+    private int clienteId;
+
+    public Pedido() {
+    }
+
+    public Pedido(int idPedido, LocalDateTime data, String status, int clienteId) {
+        this.idPedido = idPedido;
+        this.data = data;
+        this.status = status;
+        this.clienteId = clienteId;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(int clienteId) {
+        this.clienteId = clienteId;
+    }
 }
